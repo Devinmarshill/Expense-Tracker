45d025900e51f48cb235c4cede8335d398931eef
Devin <devinhill2911@gmail.com>
2023-11-20 19:53:46 -0500

    pulling up to date app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
-// import { useState } from 'react'
+
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from './components/Nav'
 import Home from './components/pages/Home'
-import "bootstrap/dist/css/bootstrap.css"
+// import "bootstrap/dist/css/bootstrap.css"
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
   <>
