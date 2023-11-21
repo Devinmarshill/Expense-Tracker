923b40c44128741b9d8be20d7639f6568cae575e
Alex Delva <alexdelva16@gmail.com>
2023-11-20 19:46:52 -0500

    added changes to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.css"
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
   <>
