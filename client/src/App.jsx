ef99bafed37555dc9ad3758609fff5cb91640b62
Alex Delva <alexdelva16@gmail.com>
2023-11-21 21:13:07 -0500

    added changes to css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import './style.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import './font-awesome.min.css'
+
 import Nav from './components/Nav'
 import Home from './components/pages/Home'
 
